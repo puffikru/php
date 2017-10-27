286517f19e4bfd5feeb3f023377b10a080d7d4a5
Bulakh Igor <bulakh.igor@gmail.com>
2017-10-28 02:41:33 +0300

    Changed UserController

--- controller/UserController.php
+++ controller/UserController.php
@@ -9,10 +9,19 @@
 namespace controller;
 
 
+use model\Texts;
+
 class UserController extends FrontController
 {
     public function signUpAction()
     {
+        $text = new Texts();
+        $msg = '';
 
+        $this->menu = $this->build('v_menu');
+        $this->sidebar = $this->build('v_left');
+        $this->texts = $text->getTexts() ?? null;
+        $this->title = 'Регистрация';
+        $this->content = $this->build('v_signup', ['msg' => $msg]);
     }
 }
