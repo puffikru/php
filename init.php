35147b672ee8bb7003050716ffba1a7e8ca2a50c
Bulakh Igor <bulakh.igor@gmail.com>
2017-11-24 16:34:27 +0300

    Added init.php and modified index.php

---
+++ init.php
@@ -0,0 +1,19 @@
+<?php
+include_once "config.php";
+
+// display errors
+ini_set('display_errors', true);
+
+// errors are equals to all except notices
+Error_Reporting(E_ALL & ~E_NOTICE);
+
+// locale is RU excluding numeric
+setlocale(LC_ALL, 'ru_RU.UTF-8');
+setlocale(LC_NUMERIC, 'C');
+
+// time zone is Moscow
+date_default_timezone_set('Europe/Moscow');
+
+// autoload classes
+include_once("autoload.php");
+include_once("vendor/autoload.php");
